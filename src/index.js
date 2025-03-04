312234da9204155360e058e8534a6596f5450a64
Ajamal <abdullahijamal08@gmail.com>
2025-03-04 22:48:44 +0000

    Bootstrap fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './css/landingPage.css';
+// Importing Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Importing Bootstrap Icons CSS
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
