c35f3d996741249ef0f3586104d6796c63b6af43
Ajamal <abdullahijamal08@gmail.com>
2025-03-04 22:21:54 +0000

    Gitignore and Npm setup

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './css/landingPage.css';
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import the plugin
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Handle CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Specify the location of your HTML file
+    }),
+  ],
+};
