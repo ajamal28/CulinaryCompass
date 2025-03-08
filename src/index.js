184805c050fdc87b960f69bae532bda9505a32ce
Ajamal <abdullahijamal08@gmail.com>
2025-03-08 12:44:34 +0000

    Login Page Addition

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/landingPage.css';
+import './css/loginPage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
